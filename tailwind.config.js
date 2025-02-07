58777b93d59d2bee112e17b22edc54597fef14e5
NISHANT4510 <nishant4510@gmail.com>
2025-02-07 23:46:18 +0530

    Path Corrected Again

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./index.html'],
+  content: [
+    "./index.html",         // Main HTML file
+    "./src/**/*.{html,js}"  // Include all HTML & JS files in src folder
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
